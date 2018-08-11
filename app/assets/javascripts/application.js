51a2912f7f1428acaa6f33296347ef01ddac5d22
hongbin <fusis1@naver.com>
2018-08-11 23:24:32 +0900

    디자인 살짝수정, 글 새로작성할 때 뒤로가기버튼 구현

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){ 
+    // bxslider 
+    var slider = $('.bxslider').bxSlider({ 
+           mode: 'horizontal', // 슬라이드 방식 ( horizontal, fade, vertical  중 선택 ) 
+           // mode: 'fade', 
+           auto: true, // 자동재생 유무 
+           controls:false, // 이전, 다음 컨트롤 유무 
+           autoHover: true, // 슬라이더 영역에 마우스 올렸을시 슬라이드를 멈출건지 여부 
+           pause: 2000, // 한 슬라이드가 멈춰있는 시간 
+           speed: 600, // 슬라이드애니메이션의 속도 
+           pager: true, // 페이징버튼 사용 여부 
+           pagerCustom: '#slider-pager', // 페이징 버튼이 들어있는 부모태그의 선택자 
+           // 버튼을 눌러 슬라이드를 이동시킨후 다시 슬라이드가 자동 재생되게 하는 옵션 
+           onSlideAfter: function($slideElement, oldIndex, newIndex){ 
+                  slider.startAuto(); 
+           } 
+    }); 
+}); 
