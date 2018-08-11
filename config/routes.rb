3ea8ccdad40ae88c6ce80d6908721e49252d899f
Lee Kwang Yong <korea3457@naver.com>
2018-08-11 19:34:01 +0900

    일러스트

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'post/index'
-
+  get 'post/notice'
   get 'post/new'
   post 'post/create'
+  get 'post/customer'
+  get 'post/mypage'
   devise_for :users
-
-
-  get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
