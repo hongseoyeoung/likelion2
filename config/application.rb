4c9b62d4f74c40973e25f3b7b7e97da74a6e0cbf
hongbin <fusis1@naver.com>
2018-08-14 03:10:21 +0900

    시간 동기화 설정

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Hongbin
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Seoul'
+    config.active_record.default_timezone = :local
   end
 end
